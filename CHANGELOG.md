89501b6b08fbaa3d7eeb88c16fc1bd59ef552370
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-12-30 14:15:47 +0100

    Bumped 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 # Changelog
 
+* `1.0.0`: stable version
 * `0.0.1`: initial commit
--- lib/SassySort.rb
+++ lib/SassySort.rb
@@ -6,8 +6,8 @@ Compass::Frameworks.register('SassySort', :path => extension_path)
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module SassySort
-  VERSION = "0.0.1"
-  DATE = "2014-02-19"
+  VERSION = "1.0.0"
+  DATE = "2014-12-30"
 end
 
 module Sass::Script::Functions
